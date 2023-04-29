f924620dc4e93c7ccfdb5f29312da8ce31339e69
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-29 21:55:09 +0200

    finished lesson

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let atoms = [];
-const randomColors = ["red", "blue", "green", "yellow", "pink", "purple"];
+const randomColors = ["white", "grey", "blue"];
 
-canvas.addEventListener("mousemove", function (e) {
-  for (let i = 0; i < 10; i++) {
-    atoms.push(new Atom(e.x, e.y));
-  }
-});
+// canvas.addEventListener("mousemove", function (e) {
+//   for (let i = 0; i < 10; i++) {
+//     atoms.push(new Atom(e.x, e.y));
+//   }
+// });
 
 const animate = () => {
   atoms.forEach((atom, index) => {
+    ctx.fillStyle = "white";
     atom.draw();
     atom.updateSpeed();
     atom.updateSize();
@@ -23,7 +24,8 @@ const animate = () => {
     }
   });
   ctx.save();
-  ctx.fillStyle = "rgba(255, 255, 24, 0.2)";
+  //   ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
+  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.restore();
   requestAnimationFrame(animate);
@@ -35,9 +37,10 @@ class Atom {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = Math.random() * 8 + 2;
-    this.speedX = Math.random() * 4 - 2;
+    this.radius = Math.random() * 1 + 2;
+    this.speedX = Math.random() * 1 - 2;
     this.speedY = Math.random() * 4 - 2;
+    //     this.color = randomColors[Math.floor(Math.random() * randomColors.length)];
   }
 
   updateSpeed() {
@@ -52,6 +55,15 @@ class Atom {
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.stroke();
+    ctx.fill();
   }
 }
+
+const generateAtoms = () => {
+  atoms.push(
+    new Atom(Math.random() * canvas.width, Math.random() * canvas.height)
+  );
+  requestAnimationFrame(generateAtoms);
+};
+
+generateAtoms();
