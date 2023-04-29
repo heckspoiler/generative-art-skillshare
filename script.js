72523fe0507c58fb325cc27c719eb46087c992ae
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-29 18:29:12 +0200

    arc also checked out

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-canvas.addEventListener("mousemove", function (e) {
-  console.log(e.x, e.y);
-  ctx.beginPath();
-  ctx.rect(e.x, e.y, 10, 10);
-  // ctx.fill();
-  ctx.stroke();
-});
+// canvas.addEventListener("mousemove", function (e) {
+//   console.log(e.x, e.y);
+//   ctx.beginPath();
+//   ctx.arc(e.x, e.y, 10, 10);
+//   // ctx.fill();
+//   ctx.stroke();
+// });
+
+const degToRad = (deg) => {
+  return (deg / 180) * Math.PI;
+};
+
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, degToRad(270));
+ctx.stroke();
