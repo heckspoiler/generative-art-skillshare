4c3b8f2087f655b09c11463d45eefa5cd5604396
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-29 18:44:22 +0200

    first animation finished

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// canvas.addEventListener("mousemove", function (e) {
-//   console.log(e.x, e.y);
-//   ctx.beginPath();
-//   ctx.arc(e.x, e.y, 10, 10);
-//   // ctx.fill();
-//   ctx.stroke();
-// });
+let atoms = [];
+const randomColors = ["red", "blue", "green", "yellow", "pink", "purple"];
 
-const degToRad = (deg) => {
-  return (deg / 180) * Math.PI;
+canvas.addEventListener("click", function (e) {
+  for (let i = 0; i < 10; i++) {
+    atoms.push(new Atom(e.x, e.y));
+  }
+});
+
+const animate = () => {
+  atoms.forEach((atom) => {
+    atom.draw();
+    atom.update();
+  });
+  requestAnimationFrame(animate);
 };
 
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, degToRad(270));
-ctx.stroke();
+animate();
+
+class Atom {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = Math.random() * 10 + 2;
+    this.speedX = Math.random() * 4 - 2;
+    this.speedY = Math.random() * 4 - 2;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
