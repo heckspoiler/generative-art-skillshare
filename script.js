97eccbe045e9c2831a58f539ecc53fcf2a155b59
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-29 18:24:07 +0200

    first canvas finished

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(ctx);
 
-ctx.beginPath();
-ctx.rect(100, 100, 100, 100);
-// ctx.fill();
-ctx.stroke();
+canvas.addEventListener("mousemove", function (e) {
+  console.log(e.x, e.y);
+  ctx.beginPath();
+  ctx.rect(e.x, e.y, 10, 10);
+  // ctx.fill();
+  ctx.stroke();
+});
